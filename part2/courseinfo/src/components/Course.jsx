5edf2b70e6f664f09f786012163e5e57f8b6efbc
ethio-man <natymiskir@gmail.com>
2026-03-02 03:32:35 -0300

    Exercise 2.1 is done

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header.jsx";
+import Content from "./Content.jsx";
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  );
+};
+
+export default Course;
