d3502d62e3f1a5160c5aaea2e29bd0a869a8de04
ethio-man <natymiskir@gmail.com>
2026-03-02 03:47:09 -0300

    Exercise 2.2 is done

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,16 +1,11 @@
 import Part from "./Part.jsx";
 function Content({ course }) {
   console.log("course", course);
-  const part1 = course.parts[0];
-  const part2 = course.parts[1];
-  const part3 = course.parts[2];
-
-  console.log("part1", part1);
   return (
     <div>
-      <Part part={part1.name} exercise={part1.exercises} />
-      <Part part={part2.name} exercise={part2.exercises} />
-      <Part part={part3.name} exercise={part3.exercises} />
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part.name} exercise={part.exercises} />
+      ))}
     </div>
   );
 }
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header.jsx";
 import Content from "./Content.jsx";
+import Total from "./Total.jsx";
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
