d92f2cdc30e56a0c4a5bbfa69f4232a4d88a006a
ethio-man <natymiskir@gmail.com>
2026-03-16 02:24:31 -0300

    Exercise 2.11 is done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { PersonForm } from "./components/PersonForm.jsx";
 import { Filter } from "./components/Filter.jsx";
 import { Persons } from "./components/Persons.jsx";
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "12-34-567890", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const persons = await axios.get("http://localhost:3001/persons");
+        setPersons(persons.data);
+      } catch (err) {
+        console.log("Error to fetch data.", err);
+      }
+    }
+    fetchData();
+  }, []);
   const [search, setSearch] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
