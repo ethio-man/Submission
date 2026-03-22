d9d2870b3593cca78d2db83ae44f82b68cd8b5d3
ethio-man <natymiskir@gmail.com>
2026-03-22 06:46:24 -0300

    Exercisese 2.20 is done

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+
 function App() {
+  const API_key = import.meta.env.VITE_WEATHER_API;
   const [countries, setCountries] = useState("");
   const [information, setInformation] = useState([]);
+  const [weather, setWeather] = useState([]);
+  const [location, setLocation] = useState([]);
+  const [temprature, setTemprature] = useState(null);
   useEffect(() => {
     axios
       .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
@@ -13,9 +18,38 @@ function App() {
       })
       .catch((err) => console.log("Error to fetch data", err));
   }, []);
+
+  useEffect(() => {
+    if (information.length === 1) {
+      const city = information[0]?.capital;
+      //searching city's cooardinate for weather api call
+      axios
+        .get(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_key}`,
+        )
+        .then((response) =>
+          setLocation([response.data[0].lat, response.data[0].lon]),
+        )
+        .catch((err) => console.log(err));
+      //searching city's weather condition useing it's coordinate
+      const lat = location[0];
+      const lon = location[1];
+      if (lat && lon) {
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`,
+          )
+          .then((response) => {
+            setWeather(response.data);
+            setTemprature(response.data?.main?.temp - 273.15);
+          })
+          .catch((err) => console.log(err));
+      }
+    }
+  }, [information]);
   const handleSearch = (e) => {
     e.preventDefault();
-    const filtered = countries.filter((c) => {
+    const filtered = countries?.filter((c) => {
       const searchLower = e.target.value.toLowerCase();
       return c.name.common.toLowerCase().includes(searchLower);
     });
@@ -24,6 +58,9 @@ function App() {
   };
 
   console.log(countries);
+  console.log("coordinate:", location);
+  console.log("Weather", weather);
+  console.log("Temprature in Kelvin ", temprature);
   return (
     <div>
       <div>
@@ -56,6 +93,8 @@ function App() {
             src={information[0]?.flags?.png}
             alt={information?.name?.common}
           />
+          <h2>Weather in {information[0]?.capital}</h2>
+          <p>Temperature {temprature?.toFixed(2)}</p>
         </div>
       )}
     </div>
